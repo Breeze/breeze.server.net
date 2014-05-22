10660172f009b98f4a04f7fb3a3db1a56a082582
Steve Schmitt <steves@ideablade.com>
2014-05-22 14:05:51 -0700

    NH: Added missing files

---
+++ Tests/Model_NorthwindIB_NH/Entities/ProductDiscontinued.cs
@@ -0,0 +1,14 @@
+﻿namespace Models.NorthwindIB.NH
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ProductDiscontinued : ProductMulti
+    {
+        public virtual Nullable<System.DateTime> DiscontinuedDate { get; set; }
+        public virtual Nullable<int> AlternateSupplierID { get; set; }
+
+        public virtual Supplier AlternateSupplier { get; set; }
+
+    }
+}
