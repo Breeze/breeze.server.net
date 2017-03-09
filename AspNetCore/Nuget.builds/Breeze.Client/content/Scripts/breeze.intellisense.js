99d382f405923c30f32e940130b6b2d6ae3c6005
Jay Traband <jtraband@comcast.net>
2017-03-09 09:53:37 -0800

    build with updated version numbers + minor assembly info fixes

--- AspNetCore/Nuget.builds/Breeze.Client/content/Scripts/breeze.intellisense.js
+++ AspNetCore/Nuget.builds/Breeze.Client/content/Scripts/breeze.intellisense.js
@@ -1,4 +1,4 @@
-// Generated on: Wed Mar 08 2017 20:53:04 GMT-0800 (Pacific Standard Time)
+// Generated on: Thu Mar 09 2017 09:45:59 GMT-0800 (Pacific Standard Time)
 intellisense.annotate(breeze.core, {
   'Enum': function() {
     /// <signature>
