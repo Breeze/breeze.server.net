dfef8d955904d0bf8ce5318c6c36756235f7ce41
Steve Schmitt <steves@ideablade.com>
2018-02-01 23:36:04 +0530

    Fix: null original values causing other original values to be reset

--- AspNet/Breeze.ContextProvider.EF6/EFContextProvider.cs
+++ AspNet/Breeze.ContextProvider.EF6/EFContextProvider.cs
@@ -423,16 +423,19 @@ namespace Breeze.ContextProvider.EF6 {
             var fieldType = originalValuesRecord.GetFieldType(ordinal);
             var originalValueConverted = ConvertValue(originalValue, fieldType);
 
-            if (originalValueConverted == null) {
-              // bug - hack because of bug in EF - see 
-              // http://social.msdn.microsoft.com/Forums/nl/adodotnetentityframework/thread/cba1c425-bf82-4182-8dfb-f8da0572e5da
-              var temp = entry.CurrentValues[ordinal];
-              entry.CurrentValues.SetDBNull(ordinal);
-              entry.ApplyOriginalValues(entry.Entity);
-              entry.CurrentValues.SetValue(ordinal, temp);
-            } else {
-              originalValuesRecord.SetValue(ordinal, originalValueConverted);
-            }
+            originalValuesRecord.SetValue(ordinal, originalValueConverted);
+
+            // Below code removed by Steve, 2018-02-01, as it seems the hack is no longer needed (and causes problems)
+            //if (originalValueConverted == null) {
+            //  // bug - hack because of bug in EF - see 
+            //  // http://social.msdn.microsoft.com/Forums/nl/adodotnetentityframework/thread/cba1c425-bf82-4182-8dfb-f8da0572e5da
+            //  var temp = entry.CurrentValues[ordinal];
+            //  entry.CurrentValues.SetDBNull(ordinal);
+            //  entry.ApplyOriginalValues(entry.Entity);
+            //  entry.CurrentValues.SetValue(ordinal, temp);
+            //} else {
+            //  originalValuesRecord.SetValue(ordinal, originalValueConverted);
+            //}
           }
         } catch (Exception e) {
           if (e.Message.Contains(" part of the entity's key")) {
