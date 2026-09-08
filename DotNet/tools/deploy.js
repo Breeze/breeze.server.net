84e1b0e5a3669e6ef4f4b0707f9d306a76597834
Steve Schmitt <steves@ideablade.com>
2026-09-08 15:54:54 -0700

    Update version to 7.5.2 for nuget deploy

--- DotNet/tools/deploy.js
+++ DotNet/tools/deploy.js
@@ -7,8 +7,8 @@ const bu = require('.\\build-utils');
 
 const localNugetCacheDir = process.env.LOCALAPPDATA + '\\NuGet\\Test';
 const userNugetCacheDir = process.env.USERPROFILE + '\\.nuget\\packages';
-const version = '7.5.1';
-const debugOrRelease = 'Debug';
+const version = '7.5.2';
+const debugOrRelease = 'Release';
 
 var baseNames = [
   'Breeze.AspNetCore.NetCore',
